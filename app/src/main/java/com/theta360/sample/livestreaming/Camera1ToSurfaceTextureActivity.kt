a544ee41b43306ec3796bfce27bcc6a2bd4c1a01
Shunichi Shinohara <shino@shiguredo.jp>
2019-04-18 09:57:56 +0900

    Depend on sora sdk by SNAPSHOT version

--- app/src/main/java/com/theta360/sample/livestreaming/Camera1ToSurfaceTextureActivity.kt
+++ app/src/main/java/com/theta360/sample/livestreaming/Camera1ToSurfaceTextureActivity.kt
@@ -45,11 +45,11 @@ class Camera1ToSurfaceTextureActivity : Activity() {
     }
 
     // Capture parameters
-    // private val shootingMode = ThetaCapturer.ShootingMode.RIC_MOVIE_PREVIEW_3840
+    // private val shootingMode = ShootingMode.RIC_MOVIE_PREVIEW_3840
     private val shootingMode = ShootingMode.RIC_MOVIE_RECORDING_4K_EQUI
-    // private val shootingMode = ThetaCapturer.ShootingMode.RIC_MOVIE_RECORDING_4K_DUAL
-    // private val shootingMode = ThetaCapturer.ShootingMode.RIC_MOVIE_RECORDING_2K_EQUI
-    // private val shootingMode = ThetaCapturer.ShootingMode.RIC_MOVIE_RECORDING_2K_DUAL
+    // private val shootingMode = ShootingMode.RIC_MOVIE_RECORDING_4K_DUAL
+    // private val shootingMode = ShootingMode.RIC_MOVIE_RECORDING_2K_EQUI
+    // private val shootingMode = ShootingMode.RIC_MOVIE_RECORDING_2K_DUAL
     // private val shootingMode = ThetaCapturer.ShootingMode.RIC_STILL_PREVIEW_1920
     private val frameRate = 30
 
