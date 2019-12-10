1777220d80928fd1059e0f06c0d7d811578d284f
Shunichi Shinohara <shino@shiguredo.jp>
2019-12-10 10:19:12 +0900

    Equi or not in shooting mode

--- app/src/main/java/com/theta360/sample/livestreaming/ShootingMode.kt
+++ app/src/main/java/com/theta360/sample/livestreaming/ShootingMode.kt
@@ -4,19 +4,20 @@ enum class ShootingMode(
         val value: String,
         val width: Int,
         val height: Int,
-        val videoSize: String
+        val videoSize: String,
+        val equi: Boolean
 ) {
-    RIC_MOVIE_PREVIEW_640("RicMoviePreview640", 640, 320, "640x320"),
-    RIC_MOVIE_PREVIEW_1024("RicMoviePreview1024", 1024, 512, "1024x512"),
-    RIC_MOVIE_PREVIEW_1920("RicMoviePreview1920", 1920, 960, "1920x960"),
-    RIC_MOVIE_PREVIEW_3840("RicMoviePreview3840", 3840, 1920, "3840x1920"),
+    RIC_MOVIE_PREVIEW_640("RicMoviePreview640", 640, 320, "640x320", true),
+    RIC_MOVIE_PREVIEW_1024("RicMoviePreview1024", 1024, 512, "1024x512", true),
+    RIC_MOVIE_PREVIEW_1920("RicMoviePreview1920", 1920, 960, "1920x960", true),
+    RIC_MOVIE_PREVIEW_3840("RicMoviePreview3840", 3840, 1920, "3840x1920", true),
 
-    RIC_MOVIE_RECORDING_4K_EQUI("RicMovieRecording4kEqui", 3840, 1920, "3840x1920"),
-    RIC_MOVIE_RECORDING_4K_DUAL("RicMovieRecording4kDual", 3840, 1920, "3840x1920"),
-    RIC_MOVIE_RECORDING_2K_EQUI("RicMovieRecording2kEqui", 1920, 960, "1920x960"),
-    RIC_MOVIE_RECORDING_2K_DUAL("RicMovieRecording2kDual", 1920, 960, "1920x960"),
+    RIC_MOVIE_RECORDING_4K_EQUI("RicMovieRecording4kEqui", 3840, 1920, "3840x1920", true),
+    RIC_MOVIE_RECORDING_4K_DUAL("RicMovieRecording4kDual", 3840, 1920, "5376x2688", false),
+    RIC_MOVIE_RECORDING_2K_EQUI("RicMovieRecording2kEqui", 1920, 960, "1920x960", true),
+    RIC_MOVIE_RECORDING_2K_DUAL("RicMovieRecording2kDual", 1920, 960, "1920x960", false),
 
-    RIC_STILL_PREVIEW_1920("RicStillPreview1920", 1920, 960, "1920x960"),
+    // RIC_STILL_PREVIEW_1920("RicStillPreview1920", 1920, 960, "1920x960"),
     // Does not exist?
     // RIC_STILL_PREVIEW_3840("RicStillPreview3840", 3840, 1920, "3840x1920")
 }
--- app/src/main/java/org/webrtc/ThetaCamera1Session.java
+++ app/src/main/java/org/webrtc/ThetaCamera1Session.java
@@ -142,17 +142,21 @@ class ThetaCamera1Session implements CameraSession {
       // This does NOT work.
       // parameters.setPreviewFpsRange(frameRate, frameRate);
 
-      parameters.set("RIC_SHOOTING_MODE", shootingMode.getValue());
-      // Any effect? At least, it seems do no harm.
       parameters.set("video-size", shootingMode.getVideoSize());
-      // parameters.set("video-size", "5376x2688");
+
+      parameters.set("RIC_SHOOTING_MODE", shootingMode.getValue());
+      if (shootingMode.getEqui()) {
+        parameters.set("RIC_PROC_STITCHING", "RicStaticStitching");
+      } else {
+        parameters.set("RIC_PROC_STITCHING", "RicNonStitching");
+      }
       // If recording-hint is set to true, camera become frozen.
       // parameters.set("recording-hint", "false");
       // It seems the same as "recording-hint" above. Do not set this true.
       // parameters.setRecordingHint(true);
 
       // parameters.set("RIC_PROC_STITCHING", "RicNonStitching");
-      parameters.set("RIC_PROC_STITCHING", "RicStaticStitching");
+      // parameters.set("RIC_PROC_STITCHING", "RicStaticStitching");
       // parameters.set("RIC_PROC_STITCHING", "RicDynamicStitchingAuto");
       // parameters.set("RIC_PROC_STITCHING", "RicDynamicStitchingSave");
       // parameters.set("RIC_PROC_STITCHING", "RicDynamicStitchingLoad");
